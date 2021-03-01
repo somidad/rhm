5d91f03b5e95fece5f6f1ec10b32894f183bac14
Seokseong Jeon <sjeon87@gmail.com>
2021-03-01 12:32:49 +0900

    Working on ChangeTable

--- src/components/ChangeTable.tsx
+++ src/components/ChangeTable.tsx
@@ -3,6 +3,9 @@ import { Button, Form, Table, TextArea } from "semantic-ui-react";
 import { Change, Enum } from "../types";
 import EnumSelector from "./EnumSelector";
 
+const COLSPAN = 4;
+const ROWSPAN = 2;
+
 type Props = {
   changeList: Change[];
   lineupList: Enum[];
@@ -14,6 +17,9 @@ export default function ChangeTable({
   changeList, lineupList, customerList,
   onChange,
 }: Props) {
+  const [description, setDescription] = useState('');
+  const [beforeChange, setBeforeChange] = useState('');
+  const [afterChange, setAfterChange] = useState('');
   const [selectedCustomerIndexList, setSelectedCustomerIndexList] = useState<number[]>([]);
 
   return (
@@ -23,10 +29,11 @@ export default function ChangeTable({
           <Table.HeaderCell>Description</Table.HeaderCell>
           <Table.HeaderCell>Before change</Table.HeaderCell>
           <Table.HeaderCell>After change</Table.HeaderCell>
-          <Table.HeaderCell rowSpan={2}>Actions</Table.HeaderCell>
+          <Table.HeaderCell>Lineup</Table.HeaderCell>
+          <Table.HeaderCell rowSpan={ROWSPAN}>Actions</Table.HeaderCell>
         </Table.Row>
         <Table.Row>
-          <Table.HeaderCell colSpan={3}>Customers</Table.HeaderCell>
+          <Table.HeaderCell colSpan={COLSPAN}>Customers</Table.HeaderCell>
         </Table.Row>
       </Table.Header>
       <Table.Body>
@@ -34,30 +41,46 @@ export default function ChangeTable({
           <Table.Cell>
             <Form>
               <Form.Field>
-                <TextArea />
+                <TextArea value={description} onChange={(e) => setDescription(e.target.value)} />
               </Form.Field>
             </Form>
           </Table.Cell>
           <Table.Cell>
             <Form>
               <Form.Field>
-                <TextArea />
+                <TextArea value={beforeChange} onChange={(e) => setBeforeChange(e.target.value)} />
               </Form.Field>
             </Form>
           </Table.Cell>
           <Table.Cell>
             <Form>
               <Form.Field>
-                <TextArea />
+                <TextArea value={afterChange} onChange={(e) => setAfterChange(e.target.value)} />
               </Form.Field>
             </Form>
           </Table.Cell>
-          <Table.Cell rowSpan={2}>
+          <Table.Cell>
+            <Form>
+              <Form.Field>
+                <select>
+                  {
+                    lineupList.map((lineup) => {
+                      const { index, name } = lineup;
+                      return (
+                        <option key={index} value={index}>{name}</option>
+                      )
+                    })
+                  }
+                </select>
+              </Form.Field>
+            </Form>
+          </Table.Cell>
+          <Table.Cell rowSpan={ROWSPAN}>
             <Button icon='plus' size='tiny' />
           </Table.Cell>
         </Table.Row>
         <Table.Row>
-          <Table.Cell colSpan={3}>
+          <Table.Cell colSpan={COLSPAN}>
             <EnumSelector enumList={customerList} selectedIndexList={selectedCustomerIndexList}
               onChange={setSelectedCustomerIndexList}
             />
