34db0e740d5108c00a8096ba742f7ad1a180a4be
Seokseong Jeon <sjeon87@gmail.com>
2021-03-01 17:52:09 +0900

    Working on release history

--- src/utils.ts
+++ src/utils.ts
@@ -1,5 +1,10 @@
 import { Change, Enum, Version } from "./types";
 
+type ReleaseHistoryPerCustomerIndexList = {
+  customerIndexList: number[];
+  releaseHistory: string;
+}
+
 function accumulateChangeList(
   changeListAccumulated: Change[], customerIndex: number,
   versionList: Version[], indexPrev: number,
@@ -37,33 +42,48 @@ export function findEmptyIndex(indexList: number[]) {
 }
 
 export function publish(versionList: Version[], index: number, customerList: Enum[]) {
-  let versionNext = versionList.find((version) => version.index === index);
-  const releaseHistoryArr: string[] = [];
-  const releaseHistoryPerCustomerMap = new Map();
+  console.log('Publishing release history...');
+  console.group();
+  const releaseHistoryPerCustomerList: ReleaseHistoryPerCustomerIndexList[] = [];
   customerList.forEach((customer) => {
+    console.log(customer.name);
+    console.group();
     const releaseHistoryArrPerCustomer = [];
     const { index: customerIndex } = customer;
+    let versionNext = versionList.find((version) => version.index === index);
     while(versionNext) {
-      const { indexPrev, changeList, releaseList } = versionNext;
+      const { name, indexPrev, changeList, releaseList } = versionNext;
+      console.log(`Version: ${name}. Previous version: ${indexPrev}`);
+      console.group();
       // Check the current version is released
       const releaseFound = releaseList.find((release) => release.customerIndexList.includes(customerIndex));
-      // If the current version is not released, find the second latest released version
+      console.log(releaseFound);
+      // If the current version is not released, it is not for the given customer
       if (!releaseFound) {
-        versionNext = versionList.find((version) => version.index === indexPrev);
-        continue;
+        versionNext = undefined;
+      } else {
+        const { pkgIndex } = releaseFound;
+        const changeListAccumulated = [
+          ...changeList.filter((change) => {
+            const { customerIndexList } = change;
+            return !customerIndexList.length || customerIndexList.includes(customerIndex);
+          }),
+        ];
+        // Accumulate unreleased versions and get the second latest released version
+        versionNext = accumulateChangeList(changeListAccumulated, customerIndex, versionList, indexPrev);
+        releaseHistoryArrPerCustomer.push({ pkgIndex, changeList: changeListAccumulated });
       }
-      const { pkgIndex } = releaseFound;
-      const changeListAccumulated = [
-        ...changeList.filter((change) => {
-          const { customerIndexList } = change;
-          return !customerIndexList.length || customerIndexList.includes(customerIndex);
-        }),
-      ];
-      // Accumulate unreleased versions and get the second latest released version
-      versionNext = accumulateChangeList(changeListAccumulated, customerIndex, versionList, indexPrev);
-      releaseHistoryArrPerCustomer.push({ pkgIndex, changeList: changeListAccumulated });
+      console.groupEnd();
     }
-    releaseHistoryPerCustomerMap.set(customerIndex, releaseHistoryArrPerCustomer.join('\n'));
+    const releaseHistoryPerCustomer = releaseHistoryArrPerCustomer.join('\n');
+    if (releaseHistoryPerCustomer) {
+      releaseHistoryPerCustomerList.push({
+        customerIndexList: [customerIndex],
+        releaseHistory: releaseHistoryPerCustomer,
+      });
+    }
+    console.groupEnd();
   });
-  return JSON.stringify(releaseHistoryPerCustomerMap, null, 2);
+  console.groupEnd();
+  return JSON.stringify(releaseHistoryPerCustomerList, null, 2);
 }
