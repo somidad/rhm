2ca90ddd339c7241d08663b02e1568001d2368ba
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-07-14 13:55:27 +0900

    Working on ChangePerReleaseTable

--- src/components/ChangePerReleaseTable.tsx
+++ src/components/ChangePerReleaseTable.tsx
@@ -1,6 +1,7 @@
-import { Table } from "antd";
-import { keyActions, keyCustomers, keyDescription, keyLineup, keyVersion, titleActions, titleCustomers, titleDescription, titleLineup, titleVersion } from "../constants";
+import { Button, Popover, Table, Typography } from "antd";
+import { keyActions, keyCustomers, keyDescription, keyLineup, keyVersion, parenError, parenNone, titleActions, titleCustomers, titleDescription, titleLineup, titleVersion } from "../constants";
 import { ChangeV2, Enum, Pkg, ReleaseV2, VersionV2 } from "../types";
+const { Text } = Typography;
 
 type ChangePerReleaseTableProps = {
   changeList: ChangeV2[];
@@ -10,6 +11,24 @@ type ChangePerReleaseTableProps = {
   releaseList: ReleaseV2[];
   versionIndex: number;
   versionList: VersionV2[];
+};
+
+type EditableCellProps = {
+  record: {
+    key: number;
+    description: string;
+    beforeChange: string;
+    afterChange: string;
+    lineupIndex: number;
+    version: number;
+  };
+  dataIndex: string;
+  children: any;
+};
+
+type PopoverContentProps = {
+  beforeChange: string;
+  afterChange: string;
 };
 
 export default function ChangePerReleaseTable({
@@ -27,7 +46,74 @@ export default function ChangePerReleaseTable({
     { key: keyLineup, dataIndex: keyLineup, title: titleLineup },
     { key: keyCustomers, dataIndex: keyCustomers, title: titleCustomers },
     { key: keyActions, dataIndex: keyActions, title: titleActions },
+  ].map((column) => {
+    const { dataIndex } = column;
+    return {
+      ...column,
+      onCell: (record: any) => ({
+        record,
+        dataIndex,
+      }),
+    };
+  });
+
+  const dataSource = [
+    ...changeList.map((change) => {
+      const { description, beforeChange, afterChange, lineupIndex, versionIndex } = change;
+      return {
+        description, beforeChange, afterChange, lineupIndex, version: versionIndex,
+      };
+    }),
   ];
 
-  return <Table columns={columns} />;
+  return (
+    <Table
+      columns={columns}
+      dataSource={dataSource}
+      components={{
+        body: {
+          cell: EditableCell,
+        },
+      }}
+      pagination={false}
+    />
+  );
+
+  function EditableCell({ record, dataIndex, children, ...restProps }: EditableCellProps) {
+    const { key, beforeChange, afterChange, lineupIndex, version: versionIndex } = record;
+    const versionFound = versionList.find((version) => version.index === versionIndex);
+    return (
+      <td {...restProps}>
+        {
+          dataIndex === keyVersion ? (
+            versionFound?.name ?? parenError
+          ) : dataIndex === keyDescription ? (
+            <Popover content={() => <PopoverContent beforeChange={beforeChange} afterChange={afterChange} />}>
+              {children}
+            </Popover>
+          ) : dataIndex === keyLineup ? (
+            lineupIndex === -1 ? parenNone : lineupList.find((lineup) => lineup.index === lineupIndex)?.name ?? parenError
+          ) : dataIndex === keyActions ? (
+            <Button>Edit</Button>
+          ) : (
+            children
+          )
+        }
+      </td>
+    )
+  }
+
+  function PopoverContent({ beforeChange, afterChange }: PopoverContentProps) {
+    return (
+      <>
+        <Text strong>Before change</Text>
+        <br />
+        {beforeChange}
+        <br />
+        <Text strong>After change</Text>
+        <br />
+        {afterChange}
+      </>
+    )
+  }
 }
