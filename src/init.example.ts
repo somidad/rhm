9dce2d432f01b8670c5e874291c79502645320b4
Seokseong Jeon <sjeon87+fork@gmail.com>
2021-07-19 22:39:36 +0900

    Add init.example.ts

---
+++ src/init.example.ts
@@ -0,0 +1,6 @@
+import { Enum, Pkg, VersionV2 } from "./types";
+
+export const customerListInit: Enum[] = [];
+export const lineupListInit: Enum[] = [];
+export const pkgListInit: Pkg[] = [];
+export const versionListInit: VersionV2[] = [];
